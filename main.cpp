f56fb62aa2c53876a430630199bd3b5598e779fa
NguyenNhat4 <123147485+NguyenNhat4@users.noreply.github.com>
2023-05-10 16:28:37 +0700

    Update menu in  main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,97 @@
 #include"yeucau.h"
-int main() {
-	int k;
-	Gh g1;
-	int n;
-	list *l=(list*)malloc(sizeof(list));
-	node *p = (node*)malloc(sizeof(node));
-	khoiTaoList(l);
-	giocuatoi h1;
-    do{
-    printf("\n===== MENU =====");
-	printf("\n 1. Tao gio hang");
-	printf("\n 2. Chon hang");
-	printf("\n 3. Xem chi tiet gio hang");
-	printf("\n 4. Xoa 1 gio hang vao vi tri bat ki");
-	printf("\n 5. Chen them 1 gio hang vao vi tri bat ki");
-	printf("\n 6. Ghi gio hang vao tep nhi phan");
-	printf("\n 7. Tinh tong tien cac san pham da tao");
-	printf("\n 8. Kiem tra xem da co so tien nao do hay chua");
-	printf("\n 9. Exit");
-	printf(" \n Ban chon? ");
-	scanf("%d",&n);
-	switch(n) {
-	case 1: taoGioHang(	h1);
-	break;
-	case 2:	
-	p=createNodegiohang(g1,h1);
-	themCuoi(p,l);
-	break;
-	case 3:	
-	xuat(l);
-	break;
-	case 4:	
-	printf("Nhap vi tri muon xoa gio hang: ");
-	scanf("%d",&k);
-	xoaDanhSachBatKy(l,k);
-	break;
-	case 5: printf("Nhap vi tri ban muon chen them gio hang: ");
-    scanf("%d",&k);
-	p=createNodegiohang(g1,h1);
-	them1SanPhamVaoViTriBatKi(l,p,k);
-	break;
-	case 6: tao1File(l,n,h1);
-	break;
-	case 7: tinhTongTienCacSP(l);
-	break;
-	case 8: timGiaTienDaNhapHayChua(l);
-		break;	
-	      }
-	}while(n!=9);
+void user_menu_input(Item item, list *shopping_list, node *p, giocuatoi customer_info) {
+    int user_input;
+    int vitri ;
+    int tieptuc = 0;
+
+    while (tieptuc != -1) {
+        menu_show(1);
+        scanf("%d", &user_input);
+        switch (user_input) {
+            case 1:
+                tieptuc = -1;
+                taogiohang(customer_info);
+                printf("\nDa tao thanh cong gio hang!!!\n");
+                break;
+            case 2:
+                printf("\n END!!!!!\n");
+                return;
+        }
+    }
+    tieptuc = 0;
+
+    while (tieptuc != -1) {
+        menu_show(2);
+        scanf("%d", &user_input);
+        xoaInputBuffer();
+        switch (user_input) {
+
+            case 1:
+                p =  createNodeSanpham(item, customer_info);
+                themCuoi(p, shopping_list);
+                break;
+            case 2:
+                xuat(shopping_list);
+                break;
+            case 3:
+                do {
+                    menu_show(3);
+                    scanf("%d", &user_input);
+                    xoaInputBuffer();
+                    switch (user_input) {
+                        case 1:
+                            printf("Nhap vi tri muon xoa gio hang: ");
+                            scanf("%d", &vitri);
+                            xoadanhsachbatky(shopping_list, vitri);
+                            break;
+                        case 2:
+
+                            printf("Nhap vi tri ban muon chen them gio hang: ");
+                            scanf("%d", &vitri);
+                            p = createNodeSanpham(item,  customer_info);
+                            them1sanphamvaovitribatki(shopping_list, p, vitri);
+                            break;
+                        case 3:
+                            tao1file(shopping_list,customer_info);
+                            break;
+                        case 4:
+                            tinhtongtiencacsp(shopping_list);
+                            break;
+                        case 5:
+                            Timgiatiendanhaphaychua(shopping_list);
+                            break;
+                        case 6:
+                            taogiohang(customer_info);
+                            printf("\nThong Tin khach hang da sua thanh cong!!!\n");
+                            break;
+                        case 7:
+                            tieptuc = 0;
+                            break;
+                        case 8:
+                            tieptuc = -1;
+                            printf("\n END!!!!!\n");
+                            return;
+                    }
+                } while (tieptuc != 0);
+                break;
+            case 4:
+                printf("\n END!!!!!\n");
+                return;
+        }
+    }
 }
+
+
+int main() {
+  
+    Item item ; // data gio hang
+    list *shopping_list=(list*)malloc(sizeof(list));
+    node *p = (node*)malloc(sizeof(node));
+    khoiTaoList(shopping_list);
+    giocuatoi  customer_info ;
+    customer_info.sosanpham = 30;
+    user_menu_input(item, shopping_list, p, customer_info);
+
+}
+
 
