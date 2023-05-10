2f0b990488170f99206f961e59895e8bcd031c12
NguyenNhat4 <123147485+NguyenNhat4@users.noreply.github.com>
2023-05-10 16:30:47 +0700

    Update yeucau.h

--- yeucau.h
+++ yeucau.h
@@ -4,36 +4,47 @@
 #include"string.h"
 #include"time.h"
 #include"stdlib.h"
-typedef struct gh{
-  char tensp[30];
+typedef struct Item{
+  char tensp[40];
   int gia;
   int soluong;
-}Gh;
+}Item;
+
+
 typedef struct Node{
-	Gh data;
-	Node *next; 
+	Item data;
+	struct Node *next;
 }node;
+
 typedef struct List{
 	node *head;
 	node *tail;
 	int n;
 }list;
-  struct giohang_st {
- char tenkhachhang[30];
- char diachi[50] ;
- int sosanpham =40;
- Gh giohang;
+
+struct giohang_cua_khach {
+char tenkhachhang[50];
+char diachi[70];
+int sosanpham;
+Item giohang;
 };
-typedef giohang_st giocuatoi;
-void khoiTaoList(list *l);
+int menu_show(int mode);
+typedef struct giohang_cua_khach giocuatoi;
+void xoaInputBuffer();
+void khoiTaoList(list *shopping_list);
 void xoaXuongDong(char x[]);
-node* createNodegiohang(Gh g, giocuatoi h);
-void themCuoi(node *p, list *l);
-void taoGioHang(giocuatoi &h );
-void xoaDanhSachBatKy(list *l, int viTri);
-void timGiaTienDaNhapHayChua( list *l);
-void tinhTongTienCacSP( list *l);
-void them1SanPhamVaoViTriBatKi(list *l, node *p,int k );
-void tao1File(list *l,int n,giocuatoi h);
-void xuat(list *l);
+node* createNodeSanpham(Item g, giocuatoi h);
+void themDau(node *p, list *shopping_list);
+void themCuoi(node *p, list *shopping_list);
+void taogiohang(giocuatoi customer_info );
+void xoaDau(list *shopping_list);
+void xoaCuoi(list *shopping_list);
+void xoadanhsachbatky(list *shopping_list, int viTri);
+void Timgiatiendanhaphaychua( list *shopping_list);
+void tinhtongtiencacsp( list *shopping_list);
+void them1sanphamvaovitribatki(list *shopping_list, node *p,int k );
+void tao1file(list *shopping_list,giocuatoi h);
+void xuat(list *shopping_list);
+
+
 
